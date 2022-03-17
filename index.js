dfe6e7fcc36427406258ce11e52984ac002d64fc
Alexandra Davis <alex.davis806@gmail.com>
2022-03-17 09:28:41 -0700

    commit tree a7b8ec75a23d3b23a76158b31a20a8a7689c696c

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+
+// for (let age =30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+// }
+
+
+const writeCards = (arr, event) => {
+    let newArr = []
+    for (let i=0; i < arr.length; i++) {
+       newArr.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArr
+}
+
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+
+const countDown = () => {
+    let countup = 10;
+    while (countup > -1)
+    console.log(countup--);
+}
+
+console.log(countDown(10));
